fa4f7a2abe953a39535cf47cb35a0229e01ce4c4
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-07-30 00:39:17 +0200

    done

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ console.log("Hello World!");
 // console.log(decToHex(2748));
 // console.log(hexToDec('2d2d2d'));
 
-let hex = 'A';
-// console.log('16 in hex is: ' + decToHex(16));
+
 
 
 // hex counter
 // current number up to 10 ex: ABC+1, +1, +1, +1...n10
-let hexCount = hexToDec('2d2d2d');
-for(let i = 0; i < 1; i++){
-  console.log(hexCount.toString(16).toUpperCase());
-  hexCount++;
-}
-
+let dec = hexToDec('2d2d2d');
+const numOfColors = dec + 10;
+let colors = [];
+for(let i = dec; i < numOfColors; i++){
+  colors.push(decToHex(i))
+}console.log(colors);
 
 
 function hexToDec(hexNum) {
@@ -38,7 +37,7 @@ function hexToDec(hexNum) {
 function decToHex(decNum) {
   // decimal to hexdecimal
   let hexNum = "";
-  while (decNum % 16 != 0) {
+  do {
     switch (decNum % 16) {
       case 10: hexNum += "A"; break;
       case 11: hexNum += "B"; break;
@@ -49,6 +48,13 @@ function decToHex(decNum) {
       default: hexNum += decNum % 16; break;
     }
     decNum = Math.floor(decNum / 16);
-  }
+  } while (decNum % 16 != 0);
   return hexNum.split("").reverse().join("");
 }
+
+// Another way
+// let hexCount = hexToDec('2d2d2d');
+// for(let i = 0; i < 1; i++){
+//   console.log(hexCount.toString(16).toUpperCase());
+//   hexCount++;
+// }
